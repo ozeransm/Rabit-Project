4778775a4d008b3dcb7ffb6600ac0d8603e21b76
Serhii Ozeran <ozeransm@gmail.com>
2024-05-01 21:55:14 +0300

    add modal, slider

---
+++ index.js
@@ -0,0 +1,34 @@
+const iconX = document.querySelector(".modal-svg-x");
+const modal = document.querySelector(".modal");
+const btnMenu = document.querySelectorAll(".btn-menu");
+const widthScreen = window.innerWidth;
+const heightScreen = window.innerHeight;
+const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+let modalContent;
+
+btnMenu.forEach((el, i) => {
+    el.addEventListener('click', function handlerBtn() {
+        modal.classList.toggle("visually-hidden");
+        switch (i) {
+            case 0: modalContent = document.querySelector(".div-modal-contact");
+                    document.documentElement.style.setProperty('--modal-width', '300px');
+                    document.documentElement.style.setProperty('--modal-height', '250px');
+                    break;
+            case 1: modalContent = document.querySelector(".div-modal-order");
+                    document.documentElement.style.setProperty('--modal-width', (widthScreen * 0.6).toFixed(0).toString() + 'px');
+                    document.documentElement.style.setProperty('--modal-height', (heightScreen * 0.6).toFixed(0).toString() + 'px');
+                    break;
+            case 2: modalContent = document.querySelector(".div-modal-galery");
+                    document.documentElement.style.setProperty('--modal-width', (widthScreen * 0.8).toFixed(0).toString() + 'px');
+                    document.documentElement.style.setProperty('--modal-height', (heightScreen * 0.8).toFixed(0).toString() + 'px');
+                    break;
+        }
+        modalContent.classList.toggle("visually-hidden");
+        console.log((widthScreen*0.6).toFixed(0).toString());
+})
+})
+
+iconX.addEventListener('click', function handlerX() {
+    modal.classList.toggle("visually-hidden");
+    modalContent.classList.toggle("visually-hidden");
+})
