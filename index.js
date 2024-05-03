6b3a6efdbdf2bbf3fef11460251235b3ed960d85
Serhii Ozeran <ozeransm@gmail.com>
2024-05-03 17:20:00 +0300

    add btn add, prepare shoping card

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const iconX = document.querySelector(".modal-svg-x");
 const modal = document.querySelector(".modal");
 const btnMenu = document.querySelectorAll(".btn-menu");
+const cardContent = document.querySelectorAll(".div-modal-card");
+
+const btnToyA = document.querySelector("#toyA");
+const toyAname = document.querySelector("#toyAname");
+const toyAprice = document.querySelector("#toyAprice");
+const toyAselect = document.querySelector("#toyAselect");
+
+const btnToyB= document.querySelector("#toyB");
+const toyBname = document.querySelector("#toyBname");
+const toyBprice = document.querySelector("#toyBprice");
+const toyBselect = document.querySelector("#toyBselect");
+
+
 const widthScreen = window.innerWidth;
 const heightScreen = window.innerHeight;
 const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+const cardOrder = [];
 let modalContent;
 
 btnMenu.forEach((el, i) => {
@@ -23,9 +37,14 @@ btnMenu.forEach((el, i) => {
                     document.documentElement.style.setProperty('--modal-width', (widthScreen * 0.8).toFixed(0).toString() + 'px');
                     document.documentElement.style.setProperty('--modal-height', (heightScreen * 0.8).toFixed(0).toString() + 'px');
                     break;
+            case 3: modalContent = document.querySelector(".div-modal-card");
+                    document.documentElement.style.setProperty('--modal-width', (widthScreen * 0.5).toFixed(0).toString() + 'px');
+                    document.documentElement.style.setProperty('--modal-height', (heightScreen * 0.5).toFixed(0).toString() + 'px');
+                    break;
+          default: modalContent = null;
         }
-        modalContent.classList.toggle("visually-hidden");
-        console.log((widthScreen*0.6).toFixed(0).toString());
+        modalContent?.classList.toggle("visually-hidden");
+        
 })
 })
 
@@ -34,12 +53,37 @@ function handlerX() {
         modalContent.classList.toggle("visually-hidden");
         document.body.style.overflow = 'auto';
 }
+function handlerBtnToy(e) {
+  let newCardOrder = [];
+  switch (e.currentTarget.id) {
+    case 'toyA':
+      newCardOrder = [...cardOrder.filter((el) => el.id !== e.currentTarget.id)];
+      cardOrder.length = 0;
+      cardOrder.push(...newCardOrder, { id: e.currentTarget.id, name: toyAname.innerHTML, quantity: toyAselect.value, price: toyAprice.innerHTML });
+     
+      break;
+    case 'toyB':
+      
+      newCardOrder = [...cardOrder.filter((el) => el.id !== e.currentTarget.id)];
+      cardOrder.length = 0;
+      cardOrder.push(...newCardOrder, { id: e.currentTarget.id, name: toyBname.innerHTML, quantity: toyBselect.value, price: toyBprice.innerHTML });
+      
+      break;
+    default: break;
+  }
+  cardContent[0].innerHTML = cardOrder.reduce((akk, el) => akk + '<p>' + el.name + el.quantity + el.price + '</p>', '');
+  console.log(cardContent[0].innerHTML);
+}
 document.addEventListener('keydown', function(event) {
   if (event.key === "Escape") {
     handlerX();
   }
 });
 iconX.addEventListener('click', handlerX);
+
+btnToyA.addEventListener('click', handlerBtnToy);
+btnToyB.addEventListener('click', handlerBtnToy);
+
 
 const swiper = new Swiper('.swiper', {
   // Optional parameters
