8f4da66266fee5b8caa659b4355a462577d27aee
Serhii Ozeran <ozeransm@gmail.com>
2024-05-19 22:14:17 +0300

    creat shoping card

--- index.js
+++ index.js
@@ -1,24 +1,114 @@
 const iconX = document.querySelector(".modal-svg-x");
 const modal = document.querySelector(".modal");
 const btnMenu = document.querySelectorAll(".btn-menu");
-const cardContent = document.querySelectorAll(".div-modal-card");
+const cardContent = document.querySelector(".div-modal-card");
 
-const btnToyA = document.querySelector("#toyA");
-const toyAname = document.querySelector("#toyAname");
-const toyAprice = document.querySelector("#toyAprice");
-const toyAselect = document.querySelector("#toyAselect");
-
-const btnToyB= document.querySelector("#toyB");
-const toyBname = document.querySelector("#toyBname");
-const toyBprice = document.querySelector("#toyBprice");
-const toyBselect = document.querySelector("#toyBselect");
-
-
+const btnAddOrder = [];
 const widthScreen = window.innerWidth;
 const heightScreen = window.innerHeight;
 const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
-const cardOrder = [];
 let modalContent;
+let cards = [];
+let shopCard = [];
+
+function createCards(modalContent) {
+    
+  function loadJSON(path, callback) {
+    fetch(path)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Не вдалося завантажити файл');
+            }
+            return response.json();
+        })
+        .then(data => {
+            callback(data);
+        })
+        .catch(error => {
+            console.error('Помилка:', error);
+        });
+}
+
+loadJSON('cards.json', function(data) {
+  
+  let card = '';
+  cards = data.cards;
+  data.cards.map(el => {
+    card += `
+                <div class="order-card">
+            <div class="initswiper">
+                <div class="swiper">
+                    <div class="swiper-wrapper">
+                                ${el.img.reduce((akk, val) => akk +
+      `<div class="swiper-slide">
+                                     <img class="sw-img" src="${val}" alt="">
+                                  </div>`
+      , '')}
+                                                
+                            </div>
+                    <div class="swiper-pagination"></div>
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-button-next"></div>
+        
+                    <div class="swiper-scrollbar"></div>
+                </div>
+            </div>
+            <div class="order-description">
+                <h3>Description:</h3>
+                <p id="toyBname">Fluffy bunny</p>
+                <span id="toyBpriceTitle">price</span>
+                <span id="toyBprice">60</span>
+                <label for="toyBselect">quantity:</label>
+                <select id="quantity-${el.id}" name="toyBselect">
+                    <option value="0">0</option>
+                    <option value="1" selected>1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+                <button type="button" id="${el.id}">Add</button>
+            </div>
+        </div>
+
+                `;
+        
+    
+  })
+  // 
+  modalContent.innerHTML = card;
+  data.cards.map((el, i) => {
+    btnAddOrder.push(document.querySelector(`#${el.id}`));
+    btnAddOrder[i].addEventListener('click', handlerBtnToy);
+    
+  })
+  
+    const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    speed: 300,
+    autoplay: true,
+    
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      },
+    });
+
+});
+}
 
 btnMenu.forEach((el, i) => {
     el.addEventListener('click', function handlerBtn() {
@@ -32,6 +122,7 @@ btnMenu.forEach((el, i) => {
             case 1: modalContent = document.querySelector(".div-modal-order");
                     document.documentElement.style.setProperty('--modal-width', (widthScreen * 0.6).toFixed(0).toString() + 'px');
                     document.documentElement.style.setProperty('--modal-height', (heightScreen * 0.6).toFixed(0).toString() + 'px');
+                    createCards(modalContent);
                     break;
             case 2: modalContent = document.querySelector(".div-modal-galery");
                     document.documentElement.style.setProperty('--modal-width', (widthScreen * 0.8).toFixed(0).toString() + 'px');
@@ -49,62 +140,44 @@ btnMenu.forEach((el, i) => {
 })
 
 function handlerX() {
-    modal.classList.toggle("visually-hidden");
-        modalContent.classList.toggle("visually-hidden");
-        document.body.style.overflow = 'auto';
+  btnAddOrder.length = 0;
+  modal.classList.toggle("visually-hidden");
+  modalContent.classList.toggle("visually-hidden");
+  document.body.style.overflow = 'auto';
 }
+
+modal.addEventListener('click', function (event) {
+    if (event.target.className === "modal") handlerX();
+});
+  
+
 function handlerBtnToy(e) {
-  let newCardOrder = [];
-  switch (e.currentTarget.id) {
-    case 'toyA':
-      newCardOrder = [...cardOrder.filter((el) => el.id !== e.currentTarget.id)];
-      cardOrder.length = 0;
-      cardOrder.push(...newCardOrder, { id: e.currentTarget.id, name: toyAname.innerHTML, quantity: toyAselect.value, price: toyAprice.innerHTML });
-     
-      break;
-    case 'toyB':
+  const quantityOrder = document.querySelector(`#quantity-${e.target.id}`);
+  console.log(quantityOrder.options[Number(quantityOrder.value)]);
+  quantityOrder.options[Number(quantityOrder.value)].selected = true;
+  const card = { ...cards.filter(el => el.id === e.target.id)[0], quantity: quantityOrder.value, summ: 0 };
+  if (shopCard.length === 0) shopCard.push(card)
+    else {
+  shopCard.map(el => {
+    if (el.id === e.target.id) {
+      shopCard = shopCard.filter(val => val.id !== e.target.id);
       
-      newCardOrder = [...cardOrder.filter((el) => el.id !== e.currentTarget.id)];
-      cardOrder.length = 0;
-      cardOrder.push(...newCardOrder, { id: e.currentTarget.id, name: toyBname.innerHTML, quantity: toyBselect.value, price: toyBprice.innerHTML });
-      
-      break;
-    default: break;
-  }
-  cardContent[0].innerHTML = cardOrder.reduce((akk, el) => akk + '<p>' + el.name + el.quantity + el.price + '</p>', '');
-  console.log(cardContent[0].innerHTML);
+    }
+  })
+  shopCard.push(card);  
 }
-document.addEventListener('keydown', function(event) {
+  shopCard = shopCard.filter(val => Number(val.quantity) !== 0);
+  shopCard.map(el => el.summ = Number(el.price) * Number(el.quantity));
+  const summ = Number(quantityOrder.value) * Number(card.price);
+  cardContent.innerHTML = shopCard.reduce((akk, val) => akk += `<h2>Name - ${val.name} Price - ${val.price} Quantity -
+      ${val.quantity} Summ - ${val.summ}</h2>`, '');
+    
+}
+
+document.addEventListener('keydown', function (event) {
   if (event.key === "Escape") {
     handlerX();
   }
+  
 });
 iconX.addEventListener('click', handlerX);
-
-btnToyA.addEventListener('click', handlerBtnToy);
-btnToyB.addEventListener('click', handlerBtnToy);
-
-
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-speed: 300,
-autoplay: true,
-  
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
